c2985171006ce6431a7beb4d08acaf0c75faf92d
Débora <deboracristinapereira123@gmail.com>
2020-11-28 18:06:21 -0300

    Variaveis iniciais

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+let allCountries = [];
+let favoriteCountries = [];
+
+let countCountries = 0;
+let countFavoriteCountries = 0;
+
+let allPopulationList = 0;
+let allFavoritePopulationList = 0;
+
+// Elementos HTML
+
+let numberAllCountries = document.querySelector('.number-total-countries');
+let numberAllFavoriteCountries = document.querySelector('.number-favorite-countries');
+
+let populationAllCountries = document.querySelector('.population-total-countries');
+let populationFavoriteCountries = document.querySelector('.population-favorite-countries');
+
+let listCountries = document.querySelector('.list-all-countries');
+let listFavoriteCountries = document.querySelector('.list-favorite-countries'); 
+
 function start() {
   getAllCountries();
 } 
